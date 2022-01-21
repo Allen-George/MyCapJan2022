d48b71807450b46d23949c5611fec62053cdf011
Allen-George <82336964+Allen-George@users.noreply.github.com>
2022-01-21 05:08:10 +0400

    Add files via upload

---
+++ 1.1 - Area of circle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+radius = float(input('Enter the radius of the circle: '))
+area = 3.14159 * radius ** 2
+print('The area of the circle with radius 1.1 is:',area)
+
+
+# In[18]:
+
+
+x = input("Input the Filename: ")
+a = x.split(".")
+print ("The extension of the file is :",(a[1]))
+
